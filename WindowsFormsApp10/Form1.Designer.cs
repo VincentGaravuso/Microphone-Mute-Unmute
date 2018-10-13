bfae9152d0056487b46f8c2a20ea71d6e93b3116
Vincent Garavuso <vinnyg96@hotmail.com>
2018-10-12 20:43:10 -0400

    Added UI: User can switch push to talk keybind to any key on their keyboard.

--- WindowsFormsApp10/Form1.Designer.cs
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -28,23 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.keyBind_Label = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Set push to talk key";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // keyBind_Label
+            // 
+            this.keyBind_Label.AutoSize = true;
+            this.keyBind_Label.Location = new System.Drawing.Point(55, 38);
+            this.keyBind_Label.Name = "keyBind_Label";
+            this.keyBind_Label.Size = new System.Drawing.Size(75, 13);
+            this.keyBind_Label.TabIndex = 1;
+            this.keyBind_Label.Text = "Current Key: A";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(184, 61);
+            this.Controls.Add(this.keyBind_Label);
+            this.Controls.Add(this.button1);
             this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label keyBind_Label;
     }
 }
 
--- WindowsFormsApp10/Form1.cs
+++ WindowsFormsApp10/Form1.cs
@@ -13,15 +13,23 @@ namespace WindowsFormsApp10
     public partial class Form1 : Form
     {
         WindowsMicrophoneMuteLibrary.WindowsMicMute micMute = new WindowsMicrophoneMuteLibrary.WindowsMicMute();
+        bool setKey = false;
+        string keyBind = "A"; //A = default keybind
+        
         public Form1()
         {
-            
             InitializeComponent();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode.ToString() == "A")
+            if (setKey == true)
+            {
+                keyBind = e.KeyCode.ToString();
+                keyBind_Label.Text = "Current Key: " + keyBind;
+                setKey = false;
+            }
+            if (e.KeyCode.ToString() == keyBind)
             {
                 micMute.MuteMic();
             }
@@ -30,10 +38,15 @@ namespace WindowsFormsApp10
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode.ToString() == "A")
+            if (e.KeyCode.ToString() == keyBind)
             {
                 micMute.UnMuteMic();
             }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            setKey = true;
         }
     }
 }
